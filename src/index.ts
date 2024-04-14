851a20f93ba53149d4797a99f66477a0f93dfee2
Andres Court <andres@andrescourt.com>
2024-04-14 13:47:22 -0500

    added more options

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,30 @@ import {Command} from "commander";
 const program = new Command();
 
 // Aadd actions onth that CLI
-program.action(() => {
-    console.log("Hello World");
-}).description("Prints Hello World");
+program
+    .argument("<string>", "string to greet")
+    .option("-c, --capitalize", "capitalize string")
+    .action((name: string, opts: {capitalize: boolean}) => {
+        const cap = opts.capitalize ? name.toUpperCase() : name;
+        console.log(`Hello ${cap}`);
+    })
+    .description("Prints a greeting");
+
+program
+    .command("add <numbers...>")
+    .action((numbers: number[]) => {
+        const sum = numbers.reduce((acc, number) => acc + number, 0);
+        console.log(`Sum: ${sum}`);
+    })
+    .description("Adds numbers and logs the result");
+
+program
+    .command("get-max-number <numbers...>")
+    .action((numbers: number[]) => {
+        const max = Math.max(...numbers);
+        console.log(`Max: ${max}`);
+    })
+    .description("Gets the max number and logs the result");
 
 // Excecute the CLI with the arguments
 program.parse(process.argv);
